fa348758d43e8c7c2942a97e86db1aceeae87f01
abdoulkarimou7 <abdoulkarimmahamadou4@gmail.com>
2026-07-12 10:05:43 +0100

    feat: add backup script

---
+++ 01-bash-linux/backup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -ueo pipefail
+ if [ $# -eq 0 ]; then
+    echo "Give the directory path or name to backup as an argument."
+    exit 1
+fi
+date=$(date +%Y-%m-%d)
+tar -czf "backup_zip_file_${date}_archive.tar.gz" "$1"
+ if [ $? -eq 0 ]; then
+    echo "Backup successful."
+    echo "Backup file created: backup_zip_file_${date}_archive.tar.gz"
+else
+    echo "Backup failed."
+    exit 1
+fi
