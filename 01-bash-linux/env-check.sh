fefa5dfe939bc81d693cb322db6f4d7f153c140f
abdoulkarimou7 <abdoulkarimmahamadou4@gmail.com>
2026-07-14 08:33:26 +0100

    feat : adding env-check script

---
+++ 01-bash-linux/env-check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "=== Environment Check ==="
+
+echo "Current user: $USER"
+echo "Current path : $PATH"
+echo "Current shell: $SHELL"
+echo "Current working directory: (HOME) $(pwd)"
